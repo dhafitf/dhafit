dd2813c941359cc6be9e2b0417c2ebc22158a535
dhafitf <dhafidfarenza@gmail.com>
2021-09-08 00:06:01 +0700

    Fix Blog and Project

---
+++ modules/project/FeaturedProject.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import P from '../pages/project/project.module.css'
+import Link from 'next/link'
+import { getAllPosts } from "../../lib/project";
+import { GetStaticProps } from 'next'
+import PostThumb from '../PostThumb';
+
+export default function FeaturedProject() {
+  return (
+  );
+}
