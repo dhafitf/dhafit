d2a0145de1c6ff0985b1b13ff410d58ab6bb29f3
Dhafitf <dhafidfarenza@gmail.com>
2021-10-26 14:49:49 +0700

    add `AdSense` tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,36 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
-import * as dotenv from 'dotenv';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
+    return initialProps;
   }
   render() {
     return (
       <Html lang="id">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9674606173604380"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
