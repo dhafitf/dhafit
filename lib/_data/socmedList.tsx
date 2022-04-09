5977dd843f5604dd320e68a1d04da2fb6bbd1867
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-04-09 21:33:14 +0700

    add lib data

---
+++ lib/_data/socmedList.tsx
@@ -0,0 +1,8 @@
+import { FaTwitter, FaFacebookF, FaInstagram, FaGithub } from "react-icons/fa";
+
+export const socmedList: any = [
+  { text: "Facebook", icon: <FaFacebookF />, href: "https://www.facebook.com/dhafid.farenza", background: "#4267B2" },
+  { text: "Instagram", icon: <FaInstagram />, href: "https://www.instagram.com/dhafitf", background: "#E1306C" },
+  { text: "Twitter", icon: <FaTwitter />, href: "https://twitter.com/DhafitF", background: "#1DA1F2" },
+  { text: "Github", icon: <FaGithub />, href: "https://github.com/dhafitf", background: "#333333" },
+];
