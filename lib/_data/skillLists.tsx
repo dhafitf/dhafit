0a739ae46bc47672abe1b929844fa02ae3e1a2f2
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-28 00:37:46 +0700

    create tech section

---
+++ lib/_data/skillLists.tsx
@@ -0,0 +1,2 @@
+export const techSkills = ["HTML5", "CSS3", "JavaScript", "TypeScript", "Node.js", "React", "Next.js", "Markdown", "Figma", "Tailwind CSS", "MongoDB", "Express"];
+export const languageSkills = ["Indonesia", "Inggris", "Jepang", "Jawa"];
