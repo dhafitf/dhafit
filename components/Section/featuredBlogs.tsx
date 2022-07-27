ed6dcd25972a1a541839f71991a5fde666e2d3a3
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-28 00:52:23 +0700

    Create featuredBlogs.tsx

---
+++ components/Section/featuredBlogs.tsx
@@ -0,0 +1,35 @@
+import { Section } from "../Layout";
+import MoreButton from "../Other/moreButton";
+import Link from "next/link";
+import { BlogMetaData } from "~/types/posts";
+import React from "react";
+
+function FeaturedBlogItem({ title, subtitle, permalink, timestamp }: BlogMetaData) {
+  return (
+    <Link href="/blog/[slug]" as={`/blog/${permalink}`}>
+      <a className="cursor-pointer rounded-md border border-[#979797] px-3 py-4 text-[#fafafa] transition-all hover:bg-light-gray">
+        <span className="text-sm text-gray">{timestamp}</span>
+        <h3 className="truncate-3 py-2 font-semibold">{title}</h3>
+        <p className="truncate-3 text-sm text-[#D0D0D0]">{subtitle}</p>
+      </a>
+    </Link>
+  );
+}
+
+type Props = {
+  blogs: BlogMetaData[];
+  withLoadmore?: boolean;
+};
+
+export default function FeaturedBlogs({ blogs, withLoadmore }: Props) {
+  return (
+    <Section id="featured-blogs" title="Blog Unggulan">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-4">
+        {blogs.map((post: BlogMetaData) => {
+          return <FeaturedBlogItem key={post.permalink} {...post} />;
+        })}
+      </div>
+      {withLoadmore && <MoreButton className="pt-6" href="/blog" name="blog" />}
+    </Section>
+  );
+}
