6defc7cc631896c424a3e4ce91966e64e7acb254
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-28 10:33:19 +0700

    change redirect button

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ export default function project() {
         <h1 className="pb-4 text-4xl font-bold">404 - Halaman tidak ditemukan.</h1>
         <p className="pb-10">Sepertinya kamu salah mengeja alamat. Mohon periksa kembali alamat halaman.</p>
       </div>
-      <Link href="/profile">
+      <Link href="/">
         <a className="rounded bg-dark-gray p-4 text-white hover:bg-light-gray">Kembali ke beranda</a>
       </Link>
     </Layout>
