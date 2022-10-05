eb14aa606b643f9c7932af7b77b77469b6838bd7
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-10-05 23:37:14 +0700

    create new footer & warning component

---
+++ components/Other/blogFooter.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { RiExternalLinkLine } from "react-icons/ri";
+import useTranslation from "~/lib/useTranslation";
+
+const BlogFooter = ({ githubFileLink }: { githubFileLink: string }) => {
+  const { locale } = useTranslation();
+
+  return (
+    <div className="flex justify-between border-t-2 border-t-[#323538] py-6 text-sm">
+      <Link href="/blog">
+        <a className="flex items-center gap-1 hover:underline">
+          <MdOutlineKeyboardBackspace />
+          {locale["blog.footer.backToBlog"]}
+        </a>
+      </Link>
+      <a target="_blank" rel="noopener noreferrer" href={githubFileLink} className="flex items-center gap-1 hover:underline">
+        {locale["blog.footer.editOnGithub"]}
+        <RiExternalLinkLine />
+      </a>
+    </div>
+  );
+};
+
+export default BlogFooter;
