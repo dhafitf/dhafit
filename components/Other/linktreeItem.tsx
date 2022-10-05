67d52bc58dacc626dc0f46a1b7b7100ee2954ea7
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-10-05 23:36:09 +0700

    some changing

--- components/Other/linktreeItem.tsx
+++ components/Other/linktreeItem.tsx
@@ -3,12 +3,10 @@ import { LinktreeProps } from "~/types/components";
 
 const LinktreeItem: React.FC<LinktreeProps> = ({ href, text, icon }) => {
   return (
-    <div>
-      <a href={href} target="_blank" rel="noopener noreferrer" className="flex items-center justify-between rounded-xl border-2 border-lavender p-3 text-white hover:bg-lavender">
-        <div className="text-sm tracking-widest">{text}</div>
-        <div className="flex">{icon}</div>
-      </a>
-    </div>
+    <a href={href} target="_blank" rel="noopener noreferrer" className="flex items-center justify-between rounded-xl border-2 border-lavender p-3 text-white hover:bg-lavender">
+      <div className="text-sm tracking-widest">{text}</div>
+      <div className="flex">{icon}</div>
+    </a>
   );
 };
 
--- components/Posts/blogItemCard.tsx
+++ components/Posts/blogItemCard.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 import { BlogCardProps } from "~/types/posts";
-import getTimestamp from "~/lib/getTimestamp";
 
 export default function BlogItemCard({ title, subtitle, permalink, timestamp }: BlogCardProps) {
-  const getDate = getTimestamp(timestamp);
-
   return (
     <Link href="/blog/[slug]" as={`/blog/${permalink}`}>
       <a className="rounded bg-dark-gray p-4 text-white transition-colors hover:bg-light-gray">
         <div className="inline-flex w-full items-center pb-1">
           <h3 className="w-full truncate py-1 text-lg font-bold">{title}</h3>
           <div className="inline-flex flex-none pl-1 font-mono text-sm text-gray">
-            <span className="hidden pr-1 md:block">{getDate.day},</span>
-            {getDate.date}
+            <span className="hidden pr-1 md:block">{timestamp}</span>
           </div>
         </div>
         <p className="truncate-2 text-sm text-[#D0D0D0]">{subtitle}</p>
