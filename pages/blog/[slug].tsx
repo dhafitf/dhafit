cf7fc012a32fcfffad97346e8e9780d5042deddb
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-10-05 23:38:35 +0700

    add i18n & edit on github button

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,15 +1,36 @@
 import { Layout } from "@components/Layout";
 import { getAllBlogs, getBlogBySlug } from "~/lib/getPosts";
 import { GetStaticProps, GetStaticPaths } from "next";
-import PostThumb from "@components/Other/postThumb";
+import { PostThumb, BlogFooter, NotTranslatedWarning } from "@components/Other";
 import { PostProps } from "~/types/posts";
 import markdownToHtml from "~/lib/markdownToHtml";
-import getTimestamp from "~/lib/getTimestamp";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import useTranslation from "~/lib/useTranslation";
 
-export default function BlogPostPage({ tags, title, subtitle, timestamp, thumb, content }: PostProps) {
-  const getDate = getTimestamp(timestamp);
+export default function BlogPostPage({ tags, title, subtitle, timestamp, thumb, content, isNotTranslated }: PostProps) {
   const BaseUrl = "https://dhafit.vercel.app";
   const ogImageUrl = thumb ? BaseUrl + thumb : `${BaseUrl}/android-chrome-192x192.png`;
+
+  const { locale } = useTranslation();
+
+  const router = useRouter();
+  const path = router.asPath.split("/")[2];
+  const githubFileLink = `https://github.com/dhafitf/dhafit/edit/master/posts/blog/${path}.md`;
+
+  useEffect(() => {
+    let newPath = path.split("-");
+    if (locale.lang === "en" && path.startsWith("id-")) {
+      newPath.shift();
+      router.push(newPath.join("-"));
+    } else if (locale.lang === "id" && !path.startsWith("id-")) {
+      newPath.unshift("id");
+      router.push(newPath.join("-"));
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [locale]);
+
   return (
     <Layout title={title} image={ogImageUrl} description={subtitle} type="article" largeImageCard={Boolean(thumb)}>
       <article className="pb-10">
@@ -18,9 +39,7 @@ export default function BlogPostPage({ tags, title, subtitle, timestamp, thumb, 
             <PostThumb src={thumb} alt={title} />
           </div>
         )}
-        <div className="pt-4 font-mono text-sm tracking-widest">
-          {getDate.day}, {getDate.date}
-        </div>
+        <div className="pt-4 font-mono text-sm tracking-widest">{timestamp}</div>
         <h1 className="py-3 text-3xl font-bold">{title}</h1>
         <div className="flex gap-2">
           {tags.map((tag: {}, index: number) => (
@@ -30,8 +49,10 @@ export default function BlogPostPage({ tags, title, subtitle, timestamp, thumb, 
           ))}
         </div>
         <div className="mb-8 mt-4 w-full border-[1px] border-[#3d3d3d]"></div>
+        {isNotTranslated && <NotTranslatedWarning />}
         <section id="article" className="prose prose-invert" dangerouslySetInnerHTML={{ __html: content }} />
       </article>
+      <BlogFooter githubFileLink={githubFileLink} />
     </Layout>
   );
 }
