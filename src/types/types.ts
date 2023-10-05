801579c77444af84091f74d3fb8277e76033ce98
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-05 15:58:05 +0700

    define project and blog card props interface

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+interface ProjectCardProps {
+  title: string
+  tag: string
+  image: string
+  href: string
+}
+
+interface BlogCardProps {
+  title: string
+  description: string
+  href: string
+}
