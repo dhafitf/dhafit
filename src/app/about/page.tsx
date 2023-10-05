c62dbec533abc019eb4a5908c93356e637656cef
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-05 15:56:34 +0700

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+import SocialsList from "@/molecules/SocialsList"
+import TechsList from "@/molecules/TechsList"
+
+const AboutPage = () => {
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="relative">
+        <h1 className="text-white font-bold text-3xl">About me</h1>
+        <p className="pt-3">
+          I&apos;m a full-stack developer based in Indonesia, experience in utilizing JavaScript and
+          TypeScript for a wide range of development projects. Currently, I work as a freelance
+          developer, taking on projects for both frontend and backend development. I have
+          approximately 2 years of experience in this field.
+        </p>
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-xl font-semibold text-white">Tech stacks I use</h3>
+        <TechsList />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-xl font-semibold text-white">Find me on</h3>
+        <SocialsList />
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/components/molecules/TechsList.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { techItems } from "~/libs/constants"
+
+const TechsList = () => {
+  return (
+    <div className="flex items-center flex-wrap gap-3">
+      {techItems.map((item, index) => {
+        const Icon = item.icon
+        return (
+          <div key={index} className="bg-baseBg rounded-lg flex items-center gap-1 py-1.5 px-2.5">
+            <Icon className="text-xl" />
+            <span className="text-sm">{item.label}</span>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default TechsList
