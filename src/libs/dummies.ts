0f60466c1b4d3120dd7362abcbdac33a2894dea6
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-05 16:01:13 +0700

    add dummies data

---
+++ src/libs/dummies.ts
@@ -0,0 +1,29 @@
+export const projects: ProjectCardProps[] = [
+  {
+    title: "YOASOBI - Tabun (Mungkin saja) Official Music Video",
+    tag: "Translations",
+    href: "/projects/1",
+    image: "/bg.jpg",
+  },
+  {
+    title: "YOASOBI - Tabun (Mungkin saja) Official Music Video",
+    tag: "Translations",
+    href: "/projects/1",
+    image: "/bg.jpg",
+  },
+]
+
+export const blogs: BlogCardProps[] = [
+  {
+    title: "Using the Spotify API with Next.js",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl turpis, rutrum non massa vel, sodales facilisis ligula.",
+    href: "/blogs/1",
+  },
+  {
+    title: "Using the Spotify API with Next.js",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl turpis, rutrum non massa vel, sodales facilisis ligula.",
+    href: "/blogs/1",
+  },
+]
