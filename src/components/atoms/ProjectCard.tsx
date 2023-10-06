56d5ad2f7aa013e1545f37587efd088dda259576
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-06 16:22:45 +0700

    `next/image` changed in version 13
    https://nextjs.org/docs/messages/next-image-upgrade-to-13

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ title, tags, image, href }: ProjectCardProps) => {
   return (
     <CustomLink href={href} className="bg-baseBg hover:bg-hoverBg rounded-lg overflow-hidden">
       <div className="aspect-video relative overflow-hidden" title={title}>
-        <Image src={image} alt="Project title" fill objectFit="cover" objectPosition="center" />
+        <Image src={image} alt="Project title" fill className="object-cover object-center" />
       </div>
       <div className="flex flex-col gap-1.5 p-4">
         <span className="text-xs font-medium uppercase tracking-wider">{tags[0]}</span>
