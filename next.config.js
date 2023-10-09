f8da60c3dc3244f7e2883c61891af055aa4f8130
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-09 11:32:34 +0700

    add youtube translations redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/to/youtube-translations",
+        destination: "https://youtube.com/playlist?list=PL4EITLGzp8_NYyi37SmIqDQPZPA70Ta6M",
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = withContentlayer(nextConfig)
