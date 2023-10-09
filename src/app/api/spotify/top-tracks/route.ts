20d9046360045599fa4401af183939a9d0c52249
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-09 17:18:45 +0700

    connect with spotify API

---
+++ src/app/api/spotify/top-tracks/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import { getTopTracks } from "~/libs/spotify"
+
+export async function GET(request: Request) {
+  try {
+    const response = await getTopTracks()
+    const { items } = await response.json()
+    const tracks: TrackItem[] = items.map((track: TrackResponseItem): TrackItem => {
+      return {
+        title: track.name,
+        album: track.album.name,
+        albumImageUrl: track.album.images[track.album.images.length - 1].url,
+        artists: track.artists.map((artist) => ({
+          name: artist.name,
+          url: `https://open.spotify.com/artist/${artist.id}`,
+        })),
+        duration: track.duration_ms,
+        songUrl: `https://open.spotify.com/track/${track.id}`,
+      }
+    })
+
+    return NextResponse.json(tracks)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ success: false, message: "Internal Server Error" }, { status: 500 })
+  }
+}
--- src/types/spotify.ts
+++ src/types/spotify.ts
@@ -1,11 +1,50 @@
-interface NowPlayingResponse {
+interface SpotifyArtist {
+  name: string
+  url: string
+}
+
+interface BaseTrack {
+  title: string
   album: string
   albumImageUrl: string
-  artists: {
-    name: string
+  artists: SpotifyArtist[]
+  songUrl: string
+}
+
+interface CurrentlyPlayingItem extends BaseTrack {
+  isPlaying: boolean
+}
+
+interface TrackItem extends BaseTrack {
+  duration: number
+}
+
+interface AlbumResponse {
+  images: {
     url: string
   }[]
-  isPlaying: boolean
-  songUrl: string
-  title: string
+  name: string
+}
+
+interface ArtisResponse {
+  name: string
+  id: string
+}
+
+interface TrackResponseItem {
+  album: AlbumResponse
+  artists: ArtisResponse[]
+  duration_ms: number
+  id: string
+  name: string
+}
+
+interface CurrentlyPlayingResponse {
+  is_playing: boolean
+  item: TrackResponseItem
+  actions: {
+    disallows: {
+      pausing?: boolean
+    }
+  }
 }
