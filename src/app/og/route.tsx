67a4fe8197690ecd53d41e94bb27750ab13a454f
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-10-10 16:51:42 +0700

    create dynamic og image

---
+++ src/app/og/route.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from "next/server"
+import { NextRequest } from "next/server"
+
+export const runtime = "edge"
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl
+  const postTitle = searchParams.get("title")
+  const font = fetch(
+    "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Ew-Y3tcoqK5.ttf"
+  ).then((res) => res.arrayBuffer())
+  const fontData = await font
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundColor: "#1c1e1f",
+        }}
+      >
+        <div
+          style={{
+            marginLeft: 190,
+            marginRight: 190,
+            display: "flex",
+            fontSize: 128,
+            fontFamily: "Montserrat",
+            fontStyle: "normal",
+            color: "white",
+            lineHeight: "160px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {postTitle}
+        </div>
+      </div>
+    ),
+    {
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: "Montserrat",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  )
+}
