491026b1197c347dc5b97372a95c804190024f98
Dhafit Farenza <dhafidfarenza@gmail.com>
2024-03-30 05:16:03 +0700

    remove src folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,7 @@ import type { Config } from "tailwindcss"
 import typography from "@tailwindcss/typography"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./contents/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./contents/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
