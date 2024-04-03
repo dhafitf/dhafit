4c4662877bf92b8a80fa2b1d212fd2772e92ffc4
Dhafit Farenza <dhafidfarenza@gmail.com>
2024-04-04 06:44:30 +0700

    fix spacing

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -2,20 +2,16 @@ import Script from "next/script"
 
 const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
   <>
-    <Script
-      async
-      src={`https://www.googletagmanager.com/gtag/js? 
-      id=${ga_id}`}
-    ></Script>
+    <Script async src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}></Script>
     <Script
       id="google-analytics"
       dangerouslySetInnerHTML={{
         __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${ga_id}');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+        
+            gtag('config', '${ga_id}');
         `,
       }}
     ></Script>
