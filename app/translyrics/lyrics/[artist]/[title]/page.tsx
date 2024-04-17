ba4bdefc09c1302dcc79a3284538fd6c08323dd5
Dhafit Farenza <dhafidfarenza@gmail.com>
2024-04-17 09:17:19 +0700

    create lyrics pages

---
+++ app/translyrics/lyrics/[artist]/[title]/page.tsx
@@ -0,0 +1,75 @@
+import React from "react"
+import { notFound } from "next/navigation"
+import type { Metadata } from "next"
+
+import { getCombinedContents, getTrackData, getTrackLyrics } from "~/libs/lyrics"
+import TranslationSection from "@/organisms/TranslationSection"
+
+interface LyricsPageProps {
+  params: {
+    artist: string
+    title: string
+  }
+}
+
+export async function generateMetadata({ params }: LyricsPageProps): Promise<Metadata | undefined> {
+  const titleParams = decodeURIComponent(params.title)
+  const track = getTrackData(params.artist, titleParams)
+  if (!track) return
+
+  const { image } = track
+  const ogImage = image ?? `https://dhafit.vercel.app/og?title=${titleParams}`
+  const romanizedTitle = track.romanizedTitle ? ` (${track.romanizedTitle})` : ""
+  const title = `${params.artist} - ${titleParams + romanizedTitle} Lyrics`
+  const description = `Lyrics for the track "${titleParams + romanizedTitle}" by ${params.artist}.`
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      title,
+      description,
+      images: [ogImage],
+    },
+  }
+}
+
+export async function generateStaticParams() {
+  const tracks = getTrackLyrics()
+
+  return tracks.map((track) => ({
+    artist: track.artists![0],
+    title: track.title!,
+  }))
+}
+
+const LyricsPage = ({ params }: LyricsPageProps) => {
+  const title = decodeURIComponent(params.title)
+  const track = getTrackData(params.artist, title)
+  if (!track) return notFound()
+
+  const lyrics = getCombinedContents([
+    `contents/lyrics/${params.artist}/${title}/${track.defaultLyricsFile}`,
+  ])
+
+  return (
+    <TranslationSection
+      track={track}
+      subtitle={track.romanizedTitle}
+      lyrics={lyrics}
+      translations={track.translations}
+      lyricsFile={`${track.defaultLyricsFile}`}
+    />
+  )
+}
+
+export default LyricsPage
---
+++ app/translyrics/page.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import type { Metadata } from "next"
+
+import { getTrackLyrics } from "~/libs/lyrics"
+import TranslyricsListSection from "@/organisms/TranslyricsListSection"
+
+export const metadata: Metadata = {
+  title: "Translation Lyrics",
+  description: "A collection of translation lyrics I've written for various tracks.",
+}
+
+const TranslyricsPage = () => {
+  const tracks = getTrackLyrics()
+  const sortedTracks = tracks.sort(
+    (a, b) => new Date(b.updatedAt!).getTime() - new Date(a.updatedAt!).getTime()
+  )
+
+  return (
+    <div className="flex flex-col gap-7">
+      <div className="relative">
+        <h1 className="text-white font-bold text-3xl">
+          Translation Lyrics{" "}
+          <span className="text-sm text-gray-400 italic font-normal">(Translyrics)</span>
+        </h1>
+        <p className="pt-3">
+          A collection of translation lyrics I&apos;ve written for various tracks. Feel free to
+          utilize the search feature below to find what you&apos;re looking for.
+        </p>
+      </div>
+      <TranslyricsListSection tracks={sortedTracks} />
+    </div>
+  )
+}
+
+export default TranslyricsPage
