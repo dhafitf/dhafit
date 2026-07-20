3dc2f76ceca4c71d05264f3c5a93175c853dda57
Dhafit Farenza <dhafidfarenza@gmail.com>
2026-07-20 13:44:04 +0700

    refactor: update font families in global styles and layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter, JetBrains_Mono } from 'next/font/google'
+import { Noto_Sans_Mono, Plus_Jakarta_Sans, Space_Grotesk } from 'next/font/google'
 import './globals.css'
 
 import Navbar from '@/blocks/navbar'
@@ -7,15 +7,21 @@ import Footer from '@/sections/footer-section'
 import cn from '~/libs/cn'
 import GoogleAnalytics from './GoogleAnalytics'
 
-const inter = Inter({
+const jakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
-  variable: '--font-inter',
+  variable: '--font-jakarta',
   display: 'swap',
 })
 
-const jetbrainsMono = JetBrains_Mono({
+const notoMono = Noto_Sans_Mono({
   subsets: ['latin'],
-  variable: '--font-jetbrains-mono',
+  variable: '--font-noto-mono',
+  display: 'swap',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
   display: 'swap',
 })
 
@@ -53,7 +59,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en' className={cn(inter.variable, jetbrainsMono.variable)}>
+    <html lang='en' className={cn(spaceGrotesk.variable, jakarta.variable, notoMono.variable)}>
       {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
         <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />
       )}
